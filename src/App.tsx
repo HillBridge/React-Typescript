dc233ded3bdd8efa997dceadfaf38924a14619bd
qiaoguosheng@pagsmile.com <294238921@qq.com>
2025-05-17 17:37:42 +0800

    增加header中的字体

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 
 import styles from './App.module.css'
+import Logo from './assets/images/logo.svg'
 import Robot from './components/robot/Robot'
 function App() {
   return (
     <>
       <div className={styles.app}>
-        <h1 className={styles.title}>Robot Friends</h1>
+        <div className={styles.appHeader}>
+          <img src={Logo} className={styles.appLogo} alt="logo" />
+          <h1 className={styles.appTitle}>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
+        </div>
         <Robot />
      </div>
     </>
