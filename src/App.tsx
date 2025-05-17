f04f858b5336822c4615fcfda96f285c2fc84deb
qiaoguosheng@pagsmile.com <294238921@qq.com>
2025-05-17 17:31:04 +0800

    网格和弹性盒布局页面

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 
 import styles from './App.module.css'
 import Robot from './components/robot/Robot'
-
 function App() {
   return (
     <>
       <div className={styles.app}>
+        <h1 className={styles.title}>Robot Friends</h1>
         <Robot />
      </div>
     </>
