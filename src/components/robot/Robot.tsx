16ecd6a093608cfdfb2f38c04cfdc3088be13bd1
qiaoguosheng@pagsmile.com <294238921@qq.com>
2025-05-19 11:00:07 +0800

    React.FC对函数组件进行类型定义以及Interface直接对函数组件的参数进行类型定义

--- src/components/robot/Robot.tsx
+++ src/components/robot/Robot.tsx
@@ -6,7 +6,9 @@ function Robot() {
     return (
       <>
         <div className={styles.robotList}>
-            {robots.map(r => <RobotItem key={r.id} id={r.id} name={r.name} email={r.email}/>)}
+            {robots.map(r => <RobotItem key={r.id} id={r.id} name={r.name} email={r.email}>
+              <button className={styles.buy}>Buy Me</button>
+            </RobotItem>)}
         </div>
       </>
     )
--- src/components/robot/RobotItem.tsx
+++ src/components/robot/RobotItem.tsx
@@ -1,23 +1,30 @@
 import styles from './Robot.module.css'
 
+// 直接对函数的参数进行类型定义
 interface RobotProps {
     id: number
     name: string
-    email: string
+  email: string,
+  children: React.ReactNode
 }
 
-function RobotItem({ id, name, email }: RobotProps) {
+function RobotItem({ id, name, email = 'no email provided', children }: RobotProps): React.ReactElement {
     return (
       <>
         <div className={styles.cardContainer}>
-            <img src={`https://robohash.org/${id}`} alt="robot" />
-            <h2>{name}</h2>
-            <p>{email}</p>
+          <div className={styles.cardImageContainer}>
+            <img src={`https://robohash.org/${id}`} alt="robot" width={350} />
+          </div>
+           <h2>{name}</h2>
+          <p>{email}</p>
+          {children}
        </div>
       </>
     )
 }
-  
+
+
+// React.FC<P> 对函数式组件进行类型定义
 
 // const Robot: React.FC<RobotProps> = ({ id, name, aa }) => {
 //     return (
