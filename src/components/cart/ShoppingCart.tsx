52fe36832fb2dd1ab221cb0be7434a793f91c6fa
qiaoguosheng@pagsmile.com <294238921@qq.com>
2025-05-20 17:48:35 +0800

    为react事件e添加类型

--- src/components/cart/ShoppingCart.tsx
+++ src/components/cart/ShoppingCart.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import styles from './ShoppingCart.module.css'
+import React from 'react';
+// import { FiShoppingCart } from "react-icons/fi";
+import styles from './ShoppingCart.module.css';
 
 interface ShoppingCartProps {
     children?: React.ReactNode
@@ -16,9 +17,18 @@ class ShoppingCart extends React.Component<ShoppingCartProps, ShoppingCartState>
             isOpen: false
         }
     }
+    // 为react事件添加类型
+    handleClickAddCart = (e: React.MouseEvent<HTMLButtonElement>) => {
+        // e.target 是事件的触发者
+        // console.log(e.target)
+        // e.currentTarget 是事件的绑定者
+        console.log(e.currentTarget)
+        // 箭头函数没有this
+        this.setState((prevState) => ({isOpen: !prevState.isOpen}))
+    }
     render() {
         return <div className={styles.cardContainer}>
-            <button className={styles.button} onClick={() => this.setState({isOpen: !this.state.isOpen})}>购物车2件</button>
+            <button className={styles.button} onClick={this.handleClickAddCart}><span>购物车2件</span></button>
             <div className={styles.cardDropDown} style={{display: this.state.isOpen ? 'block' : 'none'}}>
                 <ul>
                     <li>robot1</li>
