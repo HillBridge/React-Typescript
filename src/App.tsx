68aa9213a53f5a2165c31cecea7ec49e94c0dedc
qiaoguosheng@pagsmile.com <294238921@qq.com>
2025-05-21 11:08:00 +0800

    组件类的生命周期

--- src/App.tsx
+++ src/App.tsx
@@ -14,17 +14,45 @@ interface AppState {
 }
 
 class App extends React.Component<AppProps, AppState> { 
+  // 初始化组件的生命周期 constructor => getDerivedStateFromProps => render => componentDidMount
   constructor(props: AppProps) {
     super(props)
     this.state = {
       userList: []
     }
   }
+  // 主要是监听props的变化 来更新内部的state
+  // 会在组件实例化后 render函数执行前被调用 是类组件内部静态的生命周期
+  static getDerivedStateFromProps(props: AppProps, state: AppState): AppState {
+    return {
+      userList: []
+    }
+  }
+  // 组件挂载dom后的执行函数
   componentDidMount(): void {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(data => this.setState({userList: data}))
   }
+
+
+  // 组件更新阶段生命周期 shouldComponentUpdate ==> render ==> componentDidUpdate
+  // shouldComponentUpdate 决定是否更新组件
+  shouldComponentUpdate(nextProps: AppProps, nextState: AppState): boolean {
+    return true
+  }
+
+  // 组件更新后的回调
+  componentDidUpdate(prevProps: AppProps, prevState: AppState): void {
+    console.log('componentDidUpdate', prevProps, prevState)
+  }
+
+  // 组件卸载前的执行函数 
+  componentWillUnmount(): void {
+    console.log('componentWillUnmount')
+  }
+
+  // 组件更新 执行
   render() { 
     return (
       <div className={styles.app}>
